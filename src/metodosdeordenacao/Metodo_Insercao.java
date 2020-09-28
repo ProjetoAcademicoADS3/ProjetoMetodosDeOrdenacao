e82be9f4131d0d4c158338b568da3910b6825422
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-09-28 14:16:35 -0300

    Adicionado o método de ordenação inserção (InsertionSort)

---
+++ src/metodosdeordenacao/Metodo_Insercao.java
@@ -0,0 +1,38 @@
+/*
+Método de ordenação inserção (InsertionSort).
+ Ordenação por inserção, neste método as comparações são feitas dos últimos
+para os primeiros valores e só deve ser usado para pequenos vetores
+ */
+package metodosdeordenacao;
+
+/**
+ * @author roger
+ */
+
+public class Metodo_Insercao {
+
+    public static void main(String[] args) {
+        int[] vetor = {54, 35, 48, 62, 11, 8, 63, 24, 28, 41};
+
+        ordena_insercao(vetor);
+        for (int valores : vetor) {
+            System.out.print(valores + " ");
+        }
+    }
+
+    //aqui é construído o método de inserção
+    static void ordena_insercao(int vetor[]) {
+        int atual;
+        int i;
+        int j;
+        for (i = 1; i < vetor.length; i++) {
+            atual = vetor[i];
+            j = i - 1;
+            while (j >= 0 && atual < vetor[j]) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = atual;
+        }
+    }
+}
